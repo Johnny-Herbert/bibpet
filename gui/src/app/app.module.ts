2bfe341443fd0b6e0340015f665d3e2f0a702b18
Vitor Sousa <vss2@cin.ufpe.br>
2019-12-06 16:04:23 -0300

    Esqueleto padrao de component gerado pelo comando ng create component home

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomeComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
