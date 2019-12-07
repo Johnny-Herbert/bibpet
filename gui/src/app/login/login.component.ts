69d57a8ff059c0d110ef9709cf01f842e2f9b683
Vitor Sousa <vss2@cin.ufpe.br>
2019-12-06 23:50:09 -0300

    Implementacao da tela de Login
    A tela contem os campos necessarios para preenchimento do usuario ou administrador

--- gui/src/app/login/login.component.ts
+++ gui/src/app/login/login.component.ts
@@ -1,15 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+  private cookieValue: string;
+  constructor(private element: ElementRef, private cookieService: CookieService, private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle("Login");
+  }
+
+  loginUsuario(username: string, password: string){
+
+    var petianos = ["accs2", "jgnvs", "som3", "jhmn", "vss2"];
+
+    if (username == "admin" && password == "admin"){
+      this.cookieService.set('user', 'admin');
+      alert ("Logado como administrador");
+      window.open('http://localhost:4200/admin/', '_self');
+    } else if (petianos.includes(username)){
+      this.cookieService.set('user', username);
+      let texto = "Logado como usuário: " + username;
+      console.log(texto);
+      window.open('http://localhost:4200/books/', '_self');
+    } else {
+      alert("Login incorreto");
+    }
   }
 
 }
