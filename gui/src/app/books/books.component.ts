6cc6d1148d7eb063ceb02a2cb0cd636952ea9384
Vitor Sousa <vss2@cin.ufpe.br>
2019-12-07 00:00:21 -0300

    Implementacao da tela de livros (Books)
    A tela contem uma lista dos livros que tem na biblioteca, no formato "Livro, edicao do livro"

--- gui/src/app/books/books.component.ts
+++ gui/src/app/books/books.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Title } from "@angular/platform-browser";
+import * as livrosS from '../book/livros.json';
 
 @Component({
   selector: 'app-books',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./books.component.css']
 })
 export class BooksComponent implements OnInit {
+  drops: any;
+  _drops = livrosS;
+  logado: Object[];
+  private cookieValue: string;
 
-  constructor() { }
-
-  ngOnInit() {
+  onSelect(book){
+    let details = book.name + ' ed: ' + book.edition; 
+    //console.log("Cookie criado: ", details);
+    this.cookieService.set('bookSelected', details);
   }
 
+  sair(){
+    this.cookieService.deleteAll('/', '');
+    this.cookieService.deleteAll('/admin', '');
+    this.cookieService.deleteAll('/login', '');
+    window.open('http://localhost:4200', '_self');
+  }
+
+  constructor(private cookieService: CookieService, private titleService: Title) {
+    this.cookieService.set('bookSelected', '');
+    var bookData = this._drops.default;
+    this.logado = [];
+    this.logado.push(this.cookieService.get('user'));
+    console.log(this.logado);
+    this.drops = Object.keys(bookData).map(x => { return { name: bookData[x].name, edition: bookData[x].edition} });
+  }
+  
+  ngOnInit() {
+    this.titleService.setTitle("Books");
+  }
 }
