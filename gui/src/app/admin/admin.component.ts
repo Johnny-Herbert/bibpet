e47dfd5dd0b463b8bc0c2db06addb824a317957e
Vitor Sousa <vss2@cin.ufpe.br>
2019-12-06 23:45:57 -0300

    Implementacao da tela de Administrador
    A tela foi implementada conforme a necessidade do meu teste de aceitacao

--- gui/src/app/admin/admin.component.ts
+++ gui/src/app/admin/admin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-admin',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  private cookieValue: string;
+  constructor(private cookieService: CookieService, private titleService: Title) { }
 
-  constructor() { }
+  sair(){
+    this.cookieService.deleteAll('/', '');
+    this.cookieService.deleteAll('/admin', '');
+    this.cookieService.deleteAll('/login', '');
+    window.open('http://localhost:4200', '_self');
+    console.log('Saindo!');
+  }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.titleService.setTitle("System Management");
+    if(! (this.cookieService.get('user') == 'admin') ){
+      console.log("Acesso não autorizado! Faça o login e tente novamente!");
+      window.location.href = "http://localhost:4200"
+    }
   }
 
 }
