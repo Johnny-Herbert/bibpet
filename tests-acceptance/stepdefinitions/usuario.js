243952322443eeb7e1dce61dcbfa8a0edee1df29
Vitor Sousa Silva <vss2@ug2c21.windows.cin.ufpe.br>
2019-12-07 11:13:09 -0300

    REFACTOR do teste de aceitacao anterior
    Solucionado problema com sintaxe do protractor e correcao de "When I check..." duplicado desnecessario

--- tests-acceptance/stepdefinitions/usuario.js
+++ tests-acceptance/stepdefinitions/usuario.js
@@ -1,49 +1,43 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const cucumber_1 = require("cucumber");
-const protractor_1 = require("protractor");
+import { defineSupportCode } from 'cucumber';
+import { browser, $, element, by } from 'protractor';
 let chai = require('chai').use(require('chai-as-promised'));
 let expect = chai.expect;
-cucumber_1.defineSupportCode(function ({ Given, When, Then }) {
-    Given(/^Given I am at some Login page not logged$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.browser.get("http://localhost:4200/login");
-        yield expect(protractor_1.browser.getTitle()).to.eventually.equal('Login BibPET');
-    }));
-    Given(/^Given I am at some "([^\"]*)" page not logged$/, (bookTitle) => __awaiter(this, void 0, void 0, function* () {
-        var urlAtual = "http://localhost:4200/" + bookTitle;
-        yield protractor_1.browser.get(urlAtual);
-        yield expect(protractor_1.browser.getTitle()).to.eventually.equal(bookTitle);
-    }));
-    When(/^When I check “Peguei o livro” option$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.element(protractor_1.by.buttonText('Peguei o livro')).click();
-    }));
-    When(/^When I check “Devolver o livro” option$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.element(protractor_1.by.buttonText('Devolver o livro')).click();
-    }));
-    Then(/^The “Book status” is now “emprestado” by "([^\"]*)"$/, (bookStatus, bookActualOwner) => __awaiter(this, void 0, void 0, function* () {
-        yield expect(protractor_1.browser.getText().to.eventually.equal(bookStatus));
-        yield expect(protractor_1.browser.getText().to.eventually.equal(bookActualOwner));
-    }));
-    Then(/^The “Book status” is now “no prazo”$/, (bookStatus) => __awaiter(this, void 0, void 0, function* () {
-        yield expect(protractor_1.browser.getText().to.eventually.equal(bookStatus));
-    }));
-    Then(/^I am at System Management page$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield expect(protractor_1.browser.getTitle()).to.eventually.equal('System Management');
-    }));
-    Then(/^I am at Home page$/, () => __awaiter(this, void 0, void 0, function* () {
-        yield expect(protractor_1.browser.getTitle()).to.eventually.equal('Homepage');
-    }));
-    When(/^When I fill login as "([^\"]*)" and password "([^\"]*)"$/, (userLogin, userPassword) => __awaiter(this, void 0, void 0, function* () {
-        yield protractor_1.$("input[name='userLogin']").sendKeys(userLogin);
-        yield protractor_1.$("input[name='userPassword']").sendKeys(userPassword);
-        yield protractor_1.element(protractor_1.by.buttonText('Entrar')).click();
-    }));
+
+defineSupportCode(function ({ Given, When, Then }) {
+    Given(/^Given I am at some Login page not logged$/, async () => {
+        await browser.get("http://localhost:4200/login");
+        await expect(browser.getTitle()).to.eventually.equal('BibPET');
+    });
+
+    Given(/^Given I am at some "([^\"]*)" page not logged$/, async (bookTitle) => {
+        await browser.get("http://localhost:4200/book");
+        await expect(browser.getTitle()).to.eventually.equal(bookTitle);
+    });
+
+    When(/^When I check "([^\"]*)" option$/,  async (optionChecked) => {
+        await element(by.buttonText(optionChecked)).click();
+    });
+
+    Then(/^The “Book status” is now “emprestado” by "([^\"]*)"$/, (bookStatus, bookActualOwner) => {
+        await expect(browser.getText().to.eventually.equal(bookStatus));
+        await expect(browser.getText().to.eventually.equal(bookActualOwner));
+    });
+
+    Then(/^The “Book status” is now “no prazo”$/, async (bookStatus) => {
+        await expect(browser.getText().to.eventually.equal(bookStatus));
+    });
+
+    Then(/^I am at System Management page$/, async () => {
+        await request.get("http://localhost:4200/admin").then(body => expect(body.includes('System Management').to.equal(true)));
+    });
+
+    Then(/^I am at Home page$/, async () => {
+        await expect(browser.getTitle()).to.eventually.equal('Homepage');
+    });
+
+    When(/^When I fill login as "([^\"]*)" and password "([^\"]*)"$/, async (userLogin, userPassword) => {
+        await $("input[name='userLogin']").sendKeys(userLogin);
+        await $("input[name='userPassword']").sendKeys(userPassword);
+        await element(by.buttonText('Entrar')).click();
+    });
 });
