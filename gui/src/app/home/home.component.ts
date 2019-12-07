3b3a7caa3c37fe8b03b2210b75bc2433d27f7e63
Vitor Sousa <vss2@cin.ufpe.br>
2019-12-06 23:54:38 -0300

    Implementacao da tela Home
    A tela contem uma simples mensagem que encaminha o usuario ao Login

--- gui/src/app/home/home.component.ts
+++ gui/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
+import { Title } from "@angular/platform-browser";
 
 @Component({
   selector: 'app-home',
@@ -6,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  private cookieValue: string;
+  constructor(private cookieService: CookieService, private titleService: Title) { }
 
   ngOnInit() {
+    this.titleService.setTitle("Homepage");
+    if(this.cookieService.get('user') != ""){
+      let user = "Logado como " + this.cookieService.get('user');
+      this.titleService.setTitle(user);
+      console.log(this.cookieService.get('user'));
+      let texto = "Bem vindo de volta " + this.cookieService.get('user');
+    }
   }
 
 }
