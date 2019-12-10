6fbd56a0e0a462984c6e955516c7ddad976a7d5c
Johnny-Herbert <jhmn@cin.ufpe.br>
2019-12-10 13:36:49 -0300

    [#42] Implementando o teste de classe buscar livro por id

--- server/spec/registerCatalogedBooks.spec.ts
+++ server/spec/registerCatalogedBooks.spec.ts
@@ -40,6 +40,19 @@ describe("O cadastro de livros", () => {
 
     });
 
+    it("buscar livro corretamente", () => {
+      var newCatalogedBook = bookRegister("Introdução a algoritmos","isbn", "Samuel Oliveira", "5", "Algoritimos");
+      register.readOnly(newCatalogedBook.book.id);
+      var catalogedBook = expectOnlyOneBook();
+      expect(catalogedBook.book.id).toBe(newCatalogedBook.book.id);
+      expect(catalogedBook.book.name).toBe("Introdução a algoritmos");
+      expect(catalogedBook.book.isbn).toBe("isbn");
+      expect(catalogedBook.book.author).toBe("Samuel Oliveira");
+      expect(catalogedBook.book.edition).toBe("5");
+      expect(catalogedBook.book.type).toBe("Algoritimos");
+
+    });
+
     it("atualizar livros corretamente", () => {
       var newCatalogedBook = bookRegister("Introdução a algoritmos","isbn", "Samuel Oliveira", "5", "Algoritimos");
       bookUpdate(newCatalogedBook.book.id, "Programação", "isbn", "Johnny Herbert", "5", "IP");
