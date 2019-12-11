9da049d9cb655ca5f9f3f28656411fbc616b2e2d
Johnny-Herbert <jhmn@cin.ufpe.br>
2019-12-11 08:03:51 -0300

    [#54] Implementando component da gui de acervo

--- gui/src/app/book-search/book-search.component.ts
+++ gui/src/app/book-search/book-search.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogedBook } from '../../../../common/CatalogedBook';
+import { Book } from '../../../../common/Book';
+import { User } from '../../../../common/User';
+import { CatalogedBookService } from './book-search.service';
 
 @Component({
   selector: 'app-book-search',
@@ -6,10 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./book-search.component.css']
 })
 export class BookSearchComponent implements OnInit {
+  catalogedBooks: Array<CatalogedBook> = new Array<CatalogedBook>();
+  catalogedBook: CatalogedBook = new CatalogedBook(new User("", "", ""), new Book(1,"","","","",""));
+  constructor(private catalogedBookServer: CatalogedBookService) { }
 
-  constructor() { }
+  search(name: string, author: string, type: string) {
+    this.catalogedBookServer.search(name, author, type)
+      .subscribe(
+        bo => {
+          if (bo) {
+            this.catalogedBooks = bo;
+            this.catalogedBook = new CatalogedBook(new User("", "", ""), new Book(1,"","","","",""));
+          }
+        },
+        msg => { alert(msg.message); }
+      );
+  }
 
   ngOnInit() {
+    this.catalogedBookServer.read()
+             .subscribe(
+               as => { this.catalogedBooks = as; },
+               msg => { alert(msg.message); }
+              );
   }
 
 }
