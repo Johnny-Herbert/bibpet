a4ad9864d7ed4a02aa7cffab1fd9b358cc425ef2
Johnny-Herbert <jhmn@cin.ufpe.br>
2019-12-11 08:02:10 -0300

    [#54] Configuração par o envio de requisições

--- gui/src/app/app.module.ts
+++ gui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule }   from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +16,8 @@ import { BookSearchComponent } from './book-search/book-search.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       {
         path: 'catalogedBooks',
